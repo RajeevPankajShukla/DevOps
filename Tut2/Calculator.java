63037cb2ce90396e6bc8470ce1bc0317184b987a
RajeevPankajShukla <RajeevPankaj.Shukla@iiitb.org>
2019-01-17 10:09:35 +0530

    Multiplication added by RPS

--- Tut2/Calculator.java
+++ Tut2/Calculator.java
@@ -12,7 +12,8 @@ public class Calculator
         {
             System.out.print("1. Addition\n");
             System.out.print("2. Subtraction\n");
-            System.out.print("3. Exit\n\n");
+            System.out.print("3. Subtraction\n");
+            System.out.print("4. Exit\n\n");
             System.out.print("Enter Your Choice : ");
             choice = scan.next().charAt(0);
             switch(choice)
@@ -22,7 +23,8 @@ public class Calculator
                             b = scan.nextFloat();
                             res = a + b;
                             System.out.print("Result = " + res);
-                            break;                                                                                
+                            break;  
+
                 case '2' :  System.out.print("Enter Two Number : ");
                             a = scan.nextFloat();
                             b = scan.nextFloat();
@@ -30,7 +32,14 @@ public class Calculator
                             System.out.print("Result = " + res);
                             break;
 
-                case '3' :  System.exit(0);
+                case '3' :  System.out.print("Enter Two Number : ");
+                            a = scan.nextFloat();
+                            b = scan.nextFloat();
+                            res = a * b;
+                            System.out.print("Result = " + res);
+                            break;
+
+                case '4' :  System.exit(0);
                             break;
 
                 default :   System.out.print("Wrong Choice!!!");
